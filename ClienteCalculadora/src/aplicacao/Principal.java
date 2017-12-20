68adf169462c748bd1f3b7c624fbf8a23ba76fc5
02376421152 <02376421152@ssp-ciospgt-04.seguranca.local>
2017-12-20 09:06:46 -0300

    atualização 1.1.0

--- ClienteCalculadora/src/aplicacao/Principal.java
+++ ClienteCalculadora/src/aplicacao/Principal.java
@@ -1,11 +1,13 @@
 package aplicacao;
 
+import servico.ServicoCalculadora;
+import servico.ServicoCalculadora_Service;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author 02376421152
@@ -16,7 +18,9 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ServicoCalculadora_Service service = new ServicoCalculadora_Service();
+        ServicoCalculadora port = service.getServicoCalculadoraPort();
+        System.out.println("Soma = "+port.somar(20, 30));
     }
-    
+
 }
